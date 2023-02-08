942371a7276fa46b597a48406fad7bd9d8ac237b
Cárthach Ó Nuanáin <carthach@Carthachs-MBP.station>
2023-02-08 17:22:09 +0000

    Update GUI in constructor

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,7 +16,7 @@ BassicManagerAudioProcessorEditor::BassicManagerAudioProcessorEditor (BassicMana
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    updateGUI();
     setResizable (true, true);
 }
 
